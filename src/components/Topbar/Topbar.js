1b7855fc6e5e1b7ff3d77478d21a2dfef2ff493e
TAIGA <>
2023-11-21 11:37:50 +0900

    Topbarを変更

--- src/components/Topbar/Topbar.js
+++ src/components/Topbar/Topbar.js
@@ -1,20 +1,30 @@
 // Topbar.js
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./Topbar.css";
 
-const Topbar = () => {
+const Topbar = ({ isAuthenticated, onLogout }) => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    // ログアウト処理
+    // この例では親コンポーネントから渡された onLogout 関数を呼び出しています
+    onLogout();
+    navigate("/");
+  };
+
   return (
-    <div className="topbar">
-      <Link to="/" className="topbar-link">
-        Home
-      </Link>
-      <Link to="/login" className="topbar-link">
-        LoginAPI
-      </Link>
-      <Link to="/dashboard" className="topbar-link">
-        Dashboard
-      </Link>
+    <div className="topbar-container">
+      <div className="app-title">LoginAPI</div>
+      <nav className="nav-links">
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        {isAuthenticated ? (
+          <button onClick={handleLogout}>Logout</button>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </nav>
     </div>
   );
 };
